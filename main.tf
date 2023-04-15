e2e0227fab76fa1f3538cf35839e0878da991f57
hendersonh <hendy@hendersonhood.com>
2023-04-15 11:29:29 -0400

    change org to  hendy
    wrong org

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@ provider "aws" {
   region = var.region
 }
 
+
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/hendy/s3-webapp/aws"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
